2080f1f059cc4d26650b6ea6e7b7500755ebd711
Peter Knibbe <peter@tigerview.com>
2021-02-26 13:27:44 -0600

    Add debug lines and text files

--- Bridge/Module1.vb
+++ Bridge/Module1.vb
@@ -49,6 +49,7 @@
         End If
 
         If IO.File.Exists(configPath) Then
+            Console.WriteLine("Found Bridge Configuration File at " & configPath)
             Dim configLines As String = IO.File.ReadAllText(configPath)
             Dim iniInd As Integer = configLines.IndexOf("iniPath=") + "iniPath=".Length
             Dim nlInd As Integer = configLines.IndexOf(vbNewLine)
@@ -57,15 +58,45 @@
             Dim pathInd As Integer = "TigerViewPath=".Length
             nlInd = part2.IndexOf(vbNewLine)
             tigerPath = part2.Substring(pathInd, nlInd - pathInd)
+            Console.WriteLine("Set ini file path = " & iniPath)
+            Console.WriteLine("Set Tiger1 executable path = " & tigerPath)
+        Else
+            Console.WriteLine("Did not find bridge configuration file at " & configPath)
+            Console.WriteLine("Press Enter to Exit.")
+            Console.ReadLine()
+            Exit Sub
         End If
 
         If IO.File.Exists(iniPath) Then IO.File.Delete(iniPath)
-        IO.File.WriteAllText(iniPath, iniLines)
+        Try
+            IO.File.WriteAllText(iniPath, iniLines)
+        Catch ex As Exception
+            Console.WriteLine("Exception in attempt to write ini file: " & ex.Message)
+            Console.WriteLine("Press Enter to Exit.")
+            Console.ReadLine()
+        End Try
 
-        Dim si As New ProcessStartInfo
-        si.FileName = tigerPath
-        si.Arguments = "Slave"
-        Process.Start(si)
+        Dim iniExists As Boolean = IO.File.Exists(iniPath)
+        Dim exeExists As Boolean = IO.File.Exists(iniPath) AndAlso IO.File.Exists(tigerPath)
+        If Not iniExists Then
+            Console.WriteLine("ini file not found at " & iniPath & ".")
+        ElseIf Not exeExists Then
+            Console.WriteLine("exe file not found at " & tigerPath & ".")
+        Else
+            Console.WriteLine("ini and exe files both found. Proceeding.")
+            Dim si As New ProcessStartInfo
+            si.FileName = tigerPath
+            si.Arguments = "Slave"
+            Try
+                Process.Start(si)
+                Console.WriteLine("Started process at tigerPath.")
+            Catch ex As Exception
+                Console.WriteLine("Exception at start process: " & ex.Message)
+            End Try
+        End If
+
+        Console.WriteLine("Press Enter to Exit.")
+        Console.ReadLine()
 
     End Sub
 
